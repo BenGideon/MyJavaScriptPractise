3151289506ca33943ed73879670fe0134a0cedb5
Ben Gideon Dokiburra <benofficial715@gmail.com>
2024-10-25 21:00:30 -0500

    tip calculator question and solution has been added

--- fundamentals/script.js
+++ fundamentals/script.js
@@ -1,11 +1,12 @@
 /* Write your code below. Good luck! 🙂 */
 
+//CHALLENGE #3
+
 const mark = {
   fullName: "Mark Miller",
   mass: 78,
   height: 1.69,
   calcBMI: function () {
-
     const ans = this.mass / (this.height * this.height);
     this.bmi = ans;
     return ans;
@@ -17,18 +18,48 @@ const john = {
   mass: 92,
   height: 1.95,
   calcBMI: function () {
-
     const ans = this.mass / (this.height * this.height);
     this.bmi = ans;
     return ans;
   },
-
 };
 
 if (mark.calcBMI() > john.calcBMI()) {
-  console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})!`);
+  console.log(
+    `${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})!`
+  );
 } else {
-  console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})!`);
+  console.log(
+    `${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})!`
+  );
 }
 
+// Challenge #4
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
 
+/* Write your code below. Good luck! 🙂 */
+
+const bills = new Array(22, 295, 176, 440, 37, 105, 10, 1100, 86, 52);
+const tips = new Array();
+const totals = new Array();
+
+for (let i = 0; i < bills.length; i++) {
+  tips[i] = calcTip(bills[i]);
+  totals[i] = bills[i] + tips[i];
+}
+
+console.log(tips, totals);
+
+const calcAverage = function (bills) {
+  let sum = 0;
+
+  for (let i = 0; i < bills.length; i++) {
+    sum = sum + bills[i];
+  }
+
+  return sum / bills.length;
+};
+
+console.log(calcAverage(bills));
